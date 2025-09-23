089d9dc474d504d8a9b5099540f65adfceeb10d8
Esther <estherdominion2002@gmail.com>
2025-09-23 09:29:50 +0100

    task 2 commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Dashord
+# Header
+<img width="1897" height="171" alt="Image" src="https://github.com/user-attachments/assets/ffa6b003-a686-42a0-a292-57d00d5452ea" />
+
+## Landing Page
+<img width="1914" height="680" alt="Image" src="https://github.com/user-attachments/assets/4b0348e1-d030-453f-8555-97c18c9f9fe8" />
+
+## Darsh 
+<img width="1904" height="806" alt="Image" src="https://github.com/user-attachments/assets/9aeea5a3-129e-4f6f-854c-f369691fe6eb" />
+
+## navbar
+<img width="1902" height="177" alt="Image" src="https://github.com/user-attachments/assets/786577a1-151c-47b9-81ff-b4e39a34713f" />
+
+
+## card grid
+<img width="1899" height="824" alt="Image" src="https://github.com/user-attachments/assets/9a79684f-3525-4d97-a855-129d9ee85608" />
+
+## Magazine nav 
+<img width="1905" height="993" alt="Image" src="https://github.com/user-attachments/assets/7de5fb29-9ddf-49f3-9303-7522af033172" />
+
+## Magazin
+<img width="1905" height="987" alt="Image" src="https://github.com/user-attachments/assets/83c2620d-863e-4919-beaf-9b110f763a4f" />
+
+
+
+
---
+++ tasks/script.js
@@ -0,0 +1,59 @@
+
+        // function PersonalInformation() {
+        //     let FirstName = "Esther";
+        //     let LastName = "Egharevba";
+        //     let Age = 20;
+        //     let SchoolName = true;
+        //     let grade = A;
+        //     let isGraduated = true;
+
+
+           
+
+
+            
+        //     let Greet =  `Hello, my name ${FirstName}, lastname = ${LastName}  `; 
+        //     let information2 = ` age = i am ${Age} years old, ls student = ${SchoolName} grade = ${grade}` `graduate :`,${isGraduated};
+        //     let FullName =" Esther Egharevba"
+           
+        // }
+        
+       
+       
+        // console.log("-----------Student Profile----");
+
+        // console.log(`name:`, firstname , `last name:`, LastName).Uppercase;
+        // console.log(`my fullname are : ${FullName.length}` );
+        // console.log(`name:`, firstname , `last name:`, LastName);
+        // console.log(`age`,${Age} + 5);
+
+function run(){
+    let num1 =  Number(prompt("Enter a number")) 
+    let num2 =  Number(prompt("Enter a number"))
+
+    let Addition = num1 + num2;
+    let Subtraction = num1 - num2;
+    let Multiplication = num1 * num2;
+    let Division = num1 /  num2;
+
+
+    let output = `
+    <strong> Arithmetic operator</strong> <br><br>
+    num1 = ${num1}, num2= ${num2} <br> <br>
+    Addition (num1 + num2): ${Addition} <br>
+    Subtraction (num1 - num2): ${Subtraction} <br>
+    Multiplication (num1 * num2): ${Multiplication} <br>
+    Division (num1 / num2): ${Division} <br>
+    `;
+
+    console.log('click')
+
+
+
+    document.getElementById('operator_output').innerHTML = output
+         
+
+           
+}
+        
+
